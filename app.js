5de25acdf486c00a5a6b207c872f32b9ad2b5507
Esn41 <151942028+Esn41@users.noreply.github.com>
2026-06-10 21:19:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1070,33 +1070,7 @@ function toggleSideMenu(){
   const menu = document.getElementById("sideMenu");
   menu.classList.toggle("open");
 }
-let startX = 0;
-let historyQuestions = [];
 
-window.addEventListener("load", () => {
-  const card = document.querySelector(".question-card");
-  const questionBox = document.getElementById("question");
-
-  if(!card || !questionBox) return;
-
-  card.addEventListener("touchstart", (e) => {
-    startX = e.touches[0].clientX;
-  });
-
-  card.addEventListener("touchend", (e) => {
-    const endX = e.changedTouches[0].clientX;
-    const diff = endX - startX;
-
-    if(diff > 70){
-      historyQuestions.push(questionBox.innerText);
-      generateQuestion();
-    }
-
-    if(diff < -70 && historyQuestions.length > 0){
-      questionBox.innerText = historyQuestions.pop();
-    }
-  });
-});
   
 
   
